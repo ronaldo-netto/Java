4d8e191adff93522594913168494b02375b135a2
Ronaldo Neto <ronaldoneto227@gmail.com>
2023-11-05 15:34:44 -0300

    Questao 27 da lista 01

---
+++ src/exercicios/lista01/Ex27.java
@@ -0,0 +1,23 @@
+package exercicios.lista01;
+
+import java.util.Scanner;
+
+public class Ex27 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		double h;
+        int m;
+        
+		System.out.println("Digite a área em hectares: ");
+		h = Double.parseDouble(scan.nextLine());
+		m = (int) h * 10000;
+
+		System.out.println("A área em metros quadrados é " + m);
+
+		scan.close();
+
+	}
+
+}
