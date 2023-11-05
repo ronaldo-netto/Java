602e894523de2e2777f6dfccf169dcf959fbb5cd
Ronaldo Neto <ronaldoneto227@gmail.com>
2023-11-05 14:32:55 -0300

    Questao 22 da lista 01

--- src/exercicios/lista01/Ex22.java
+++ src/exercicios/lista01/Ex22.java
@@ -5,18 +5,18 @@ import java.util.Scanner;
 public class Ex22 {
 
 	public static void main(String[] args) {
-		
-		Scanner scan = new Scanner (System.in);
-		
-	    double j , m;
-	    
-	    System.out.println("Digite um comprimento em jardas: ");
-	    j = Double.parseDouble(scan.nextLine());
-	    
-	    m = 0.95 * j;
-	    
-	    System.out.println("O comprimento convertido para metros é: " + m);
-	    
+
+		Scanner scan = new Scanner(System.in);
+
+		double j, m;
+
+		System.out.println("Digite um comprimento em jardas: ");
+		j = Double.parseDouble(scan.nextLine());
+
+		m = 0.91 * j;
+
+		System.out.println("O comprimento convertido para metros é: " + m);
+
 		scan.close();
 
 	}
