767e2fd47a39c173fd65c26351cc8fbc6b01016b
Ronaldo Neto <ronaldoneto227@gmail.com>
2023-11-07 20:11:55 -0300

    Questao 33 da lista 01

---
+++ src/exercicios/lista01/Ex33.java
@@ -0,0 +1,23 @@
+package exercicios.lista01;
+
+import java.util.Scanner;
+
+public class Ex33 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+
+		int l, a;
+
+		System.out.println("Digite o tamanho de um lado do quadrado: ");
+		l = scan.nextInt();
+		a = l * l;
+
+		System.out.println("A área do quadrado é: %.2f" + a);
+
+		scan.close();
+
+	}
+
+}
