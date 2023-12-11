834326a4fa268a12d3e5ada222209068a35c6f91
Ronaldo Neto <ronaldoneto227@gmail.com>
2023-12-10 21:45:47 -0300

    Questao 02 da lista 02

--- src/exercicios/lista02/Ex02.java
+++ src/exercicios/lista02/Ex02.java
@@ -5,18 +5,26 @@ import java.util.Scanner;
 public class Ex02 {
 
 	public static void main(String[] args) {
-		
-		double numero; 
-		
-		Scanner scan = new Scanner(System.in);
-		
-		System.out.println("Digite um número real: ");
-		numero = Double.parseDouble(scan.nextLine());
-		
-		System.out.println("O número digitado foi " + numero);
-		
-		scan.close();
 
+		double numero = 0, raizQuadrada;
+
+		Scanner scan = new Scanner(System.in);
+
+		do {
+
+			System.out.println("Digite um número: ");
+			numero = Double.parseDouble(scan.nextLine());
+
+			if (numero > 0.0) {
+				raizQuadrada = Math.sqrt(numero);
+				System.out.printf("A raiz quadrada de %.2f é aproximadamente %.4f%n", numero, raizQuadrada);
+			} else {
+				System.out.println("Número inválido!");
+			}
+
+		} while (numero <= 0.0);
+
+		scan.close();
 	}
 
 }
