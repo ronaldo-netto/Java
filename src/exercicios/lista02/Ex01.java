f4d6c3134774efc90647975a356b8a8f857501a6
Ronaldo Neto <ronaldoneto227@gmail.com>
2023-12-10 21:21:56 -0300

    Questao 01 da lista 02

--- src/exercicios/lista02/Ex01.java
+++ src/exercicios/lista02/Ex01.java
@@ -5,18 +5,24 @@ import java.util.Scanner;
 public class Ex01 {
 
 	public static void main(String[] args) {
-		
-		int numero; 
-		
-		Scanner scan = new Scanner(System.in);
-		
-		System.out.println("Digite um número inteiro: ");
-		numero = Integer.parseInt(scan.nextLine());
-		
-		System.out.println("O número digitado foi " + numero);
-		
-		scan.close();
 
+		double n1, n2;
+
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("Digite o primeiro número: ");
+		n1 = Double.parseDouble(scan.nextLine());
+
+		System.out.println("Digite o segundo número: ");
+		n2 = Double.parseDouble(scan.nextLine());
+
+		if (n1 > n2) {
+			System.out.println("O número " + n1 + " é o maior");
+		} else {
+			System.out.println("O número " + n2 + " é o maior");
+		}
+
+		scan.close();
 	}
 
 }
