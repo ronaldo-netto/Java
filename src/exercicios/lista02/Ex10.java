f669126b8e8b071e49b82cb8e70bcac37d2d80d5
Ronaldo Neto <ronaldoneto227@gmail.com>
2023-12-20 21:47:48 -0300

    Questao 10 da lista 02

---
+++ src/exercicios/lista02/Ex10.java
@@ -0,0 +1,32 @@
+package exercicios.lista02;
+
+import java.util.Scanner;
+
+public class Ex10 {
+
+	public static void main(String[] args) {
+
+		double h, pesoIdeal;
+		String sexo;
+
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("Digite sua altura: ");
+		h = Double.parseDouble(scan.nextLine());
+
+		System.out.println("Digite seu sexo: 'M' ou 'F' ");
+		sexo = scan.nextLine();
+
+		if (sexo.equalsIgnoreCase("M")) {
+			pesoIdeal = (72.7 * h) - 58;
+			System.out.printf("Seu peso ideal é  %.2f kg", pesoIdeal);
+		} else {
+			pesoIdeal = (62.1 * h) - 44.7;
+			System.out.println("Seu peso ideal é " + pesoIdeal);
+		}
+
+		scan.close();
+
+	}
+
+}
